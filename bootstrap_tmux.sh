570830520f635e2d793a113a7e5ec6cd7d475894
Thang Nguyen <thang@thangnguyen.us>
2021-11-11 12:05:53 -0800

    updated tmux

---
+++ bootstrap_tmux.sh
@@ -0,0 +1,3 @@
+# Helper script to bootstrap tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Afterwards call prefix+I in a tmux session to install the plugins
